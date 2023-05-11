b6336724640af6820f2c2255ebb27d05f206980a
Gabriel Bruno Traldi <traldi13@hotmail.com>
2023-05-11 17:16:55 -0300

    feat: add useEffect to hide warning when work client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+import { useEffect, useState } from 'react';
 import Modal from './components/Modal';
 
 export default function Home() {
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+   setModal(true) 
+  }, [])
   return (
-    <Modal title='teste' description='teste teste' open>
+    <Modal title='teste' description='teste teste' open={modal}>
       <p className='text-gray-700 text-base'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
         quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
