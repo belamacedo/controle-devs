4e56285e873bf81c619d006471b4c4d0c08df4f1
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-05-11 09:56:35 -0300

    fix: inserted onClick function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Dropdown from './components/Dropdown';
+import Dropdown from "./components/Dropdown";
+import Modal from "./components/Modal";
 
 export default function Home() {
-  return <Dropdown />;
+  return (
+    <Modal title="teste" description="teste teste" open>
+      <p className="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+        nihil.
+      </p>
+    </Modal>
+  );
 }
