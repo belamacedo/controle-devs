3745ba1b3944942f28a49daaa66de826ca49c47f
isabelamacedo <isabelacb.macedo@gmail.com>
2023-05-17 18:16:14 -0300

    feat: darkmodetoggle updated

--- src/app/components/Header/DarkModeToggle/index.tsx
+++ src/app/components/Header/DarkModeToggle/index.tsx
@@ -6,16 +6,7 @@ import { SunIcon, MoonIcon } from '@radix-ui/react-icons';
 import * as Styles from './styles';
 
 const DarkModeToggle = () => {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   const isDarkMode = theme === 'dark';
 
