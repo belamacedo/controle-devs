46928a04b3197c87801b4d8758b4705d884228c0
isabelamacedo <isabelacb.macedo@gmail.com>
2023-05-18 11:50:45 -0300

    styles: updated header styles

--- src/app/components/Header/UserActions/MenuLogout/styles.ts
+++ src/app/components/Header/UserActions/MenuLogout/styles.ts
@@ -9,9 +9,11 @@ export const menuButton = cva(
 export const menuImg = cva('h-8 w-8 rounded-full');
 
 export const menuItems = cva(
-  'transition ease-out duration-100 transform absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+  'transition ease-out duration-100 transform absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-zinc-900 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
 );
 
-export const menuItem = cva('block px-4 py-2 text-sm text-gray-700');
+export const menuItem = cva(
+  'block px-4 py-2 text-sm text-gray-700 dark:text-white dark:hover:bg-zinc-800'
+);
 
 export const menuItemActive = cva('bg-gray-100');
