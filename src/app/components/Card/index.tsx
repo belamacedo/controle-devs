ed7ae9eb59b15ec86288f36bea6ba552ae2b8cb6
isabelamacedo <isabelacb.macedo@gmail.com>
2023-06-16 15:02:32 -0300

    feat: homepage created

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -8,9 +8,10 @@ import * as Styles from './styles';
 
 export interface CardProps {
   title: ReactNode | string;
-  children: ReactNode;
+  subtitle: string;
   image: string;
   skills: string[];
+  className?: string;
   moreDetails?: boolean;
   onClick: () => void;
   onChange: () => void;
@@ -18,46 +19,56 @@ export interface CardProps {
 }
 export const Card = ({
   title,
-  children,
+  subtitle,
   image,
   skills,
   onChange,
   onDelete,
   onClick,
   moreDetails,
-}: CardProps) => (
-  <div className={Styles.container()}>
-    <div className={Styles.actions()}>
-      <Button
-        className={Styles.EditAndDeleteButtons()}
-        onClick={onChange}
-        icon={<Pencil2Icon className={Styles.EditAndDeleteIcons()} />}
-      ></Button>
-      <Button
-        className={Styles.EditAndDeleteButtons()}
-        onClick={onDelete}
-        icon={<TrashIcon className={Styles.EditAndDeleteIcons()} />}
-      ></Button>
-    </div>
-    <div className={Styles.imageContainer()}>
-      <img className={Styles.imageContent()} alt='profile' src={image} />
-    </div>
+  className,
+}: CardProps) => {
+  const skillsContainerHeight = skills.length > 6 ? '28' : 'auto';
 
-    <div className={Styles.title()}>{title}</div>
-    <div className={Styles.children()}>{children}</div>
-    <div className={Styles.content()}>
-      <div className={Styles.skillsContent()}>
-        {skills?.map((skill) => (
-          <span key={skill} className={Styles.skills()}>
-            {skill}
-          </span>
-        ))}
+  return (
+    <div className={`${Styles.container()} ${className}`}>
+      <div className={Styles.actions()}>
+        <Button
+          className={Styles.EditAndDeleteButtons()}
+          onClick={onChange}
+          icon={<Pencil2Icon className={Styles.EditAndDeleteIcons()} />}
+        ></Button>
+        <Button
+          className={Styles.EditAndDeleteButtons()}
+          onClick={onDelete}
+          icon={<TrashIcon className={Styles.EditAndDeleteIcons()} />}
+        ></Button>
       </div>
-      {moreDetails && (
-        <div className={Styles.moreDetails()}>
-          <Button size={"small"} text="Mais detalhes" onClick={onClick} />
+      <div className={Styles.content()}>
+        <div className={Styles.imageContainer()}>
+          <img className={Styles.imageContent()} alt='profile' src={image} />
         </div>
-      )}
+
+        <div className={Styles.title()}>{title}</div>
+        <div className={Styles.subtitle()}>{subtitle}</div>
+
+        <div
+          className={`${Styles.skillsContainer()} h-${skillsContainerHeight}`}
+        >
+          <div className={Styles.skillsContent()}>
+            {skills?.map((skill) => (
+              <span key={skill} className={Styles.skills()}>
+                {skill}
+              </span>
+            ))}
+          </div>
+        </div>
+        {moreDetails && (
+          <div className={Styles.moreDetails()}>
+            <Button size={'small'} text='Mais detalhes' onClick={onClick} />
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { Card } from '@/app/components/Card';
-import { Button, Filter, Input } from '@controle-devs-ui/react';
-import { FaSearch } from 'react-icons/fa';
 import axios from 'axios';
+import { Card } from '@/app/components/Card';
+import { Button, SearchInput } from '@controle-devs-ui/react';
+import * as Styles from './styles';
 
 interface UserInfo {
   id: number;
   imagePath: string;
   fullName: string;
+  jobPosition: string;
   hardSkills: string[];
   bio: string;
 }
 
 const MAX_CARDS = 6;
 
-const HomePage: React.FC = () => {
+const HomePage = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [data, setData] = useState<UserInfo[]>([]);
   const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:3001/users')
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json);
+    axios
+      .get('http://localhost:3001/users')
+      .then((response) => {
+        setData(response.data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   const handleShowAll = () => {
@@ -37,11 +40,9 @@ const HomePage: React.FC = () => {
   };
 
   const handleCardDelete = (id: number) => {
-    // Remove o card da tela
     const updatedData = data.filter((item) => item.id !== id);
     setData(updatedData);
 
-    // Envia a requisição de exclusão para o json-server
     axios
       .delete(`http://localhost:3001/users/${id}`)
       .then((response) => {
@@ -69,66 +70,46 @@ const HomePage: React.FC = () => {
     });
 
   const visibleData = showAll ? filteredData : filteredData.slice(0, MAX_CARDS);
+
+  const getCardClassName = (index: number) => {
+    return index === 0 ? 'mt-4' : '';
+  };
+
   const loadingIndicator = data.length === 0 ? <div>Carregando...</div> : null;
 
-  const hardSkillsOptions = [
-    ...new Set(data.flatMap((item) => item.hardSkills)),
-  ].map((skill) => ({
-    value: skill,
-    label: skill,
-  }));
-
-  const filterOptions = [
-    {
-      label: 'HardSkills',
-      options: hardSkillsOptions,
-    },
-  ];
-
   return (
-    <div className='mt-4'>
-      <div className='flex items-center justify-center mb-4'>
-        <div className='relative flex items-center mr-4'>
-          <Input
-            type='text'
-            placeholder='Pesquisar'
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <FaSearch className='absolute right-3 text-gray-500' />
-        </div>
-        <Filter
-          filterOptions={filterOptions}
-          items={data.map((item) => item.fullName)}
-          onFilter={() => console.log('onFilter')}
-          buttonText='Filtrar'
+    <div className={Styles.homeContainer()}>
+      <div className={Styles.inputContainer()}>
+        <SearchInput
+          placeholder={'Pesquisar'}
+          onChange={(value) => setSearchQuery(value)}
         />
       </div>
-      <div className='flex flex-wrap justify-center mb-4 space-x-4 space-y-4 list-container'>
+      <div className={Styles.cardContainer()}>
         {loadingIndicator}
         {filteredData &&
-          visibleData.map((item) => (
+          visibleData.map((item, index) => (
             <Card
               moreDetails={true}
               key={item.id}
               title={item.fullName}
+              subtitle={item.jobPosition}
               image={item.imagePath}
               skills={item.hardSkills}
               onClick={() => console.log('onClick')}
               onChange={() => console.log('onChange')}
               onDelete={() => handleCardDelete(item.id)}
-            >
-              {item.bio}
-            </Card>
+              className={getCardClassName(index)}
+            />
           ))}
       </div>
       {!showAll && filteredData.length > MAX_CARDS && (
-        <div className='flex justify-center mt-4 mb-4'>
+        <div className={Styles.button()}>
           <Button text='Ver mais' onClick={handleShowAll} />
         </div>
       )}
       {showAll && (
-        <div className='flex justify-center mt-4 mb-4'>
+        <div className={Styles.button()}>
           <Button text='Ver menos' onClick={handleShowLess} />
         </div>
       )}
