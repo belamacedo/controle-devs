75fb91d292611b5569c9e6cf61f88afe6ca20a2a
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-07-19 17:15:40 -0300

    fix: removed fixed height and width

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 export const container = cva(
-  'relative flex items-center justify-center  bg-white dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 rounded overflow-hidden shadow-lg px-6 py-4',
+  'grid grid-cols-1 pt-8 w-1/4  bg-white dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 rounded overflow-hidden shadow-lg px-6 py-4',
 );
 export const actions = cva('flex justify-end pb-4');
 
@@ -13,6 +13,6 @@ export const EditAndDeleteIcons = cva(
   'w-5 h-5 inline-block hover:text-purple-600 text-purple-700 dark:text-blue-700',
 );
 
-export const content = cva('absolute bottom-0 left-0 right-0 p-6');
+export const content = cva('flex flex-col w-full');
 
 export const moreDetails = cva('flex justify-end mb-4 mt-4 px-4');
